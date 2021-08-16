ce376968a5c8e706bcf39271083f28fd6ce99f92
Roxana Cretu <cretu.roxanna@gmail.com>
2021-08-16 15:32:03 +0300

    implemented CRUD methods in taskRepository

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,11 @@ package org.example;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
+import org.example.domain.Task;
 import org.example.persistance.TaskRepository;
 import org.example.transfer.CreateTaskRequest;
+import org.example.transfer.UpdateTaskRequest;
 
 /**
  * Hello world!
@@ -19,5 +22,15 @@ public class App {
 
     TaskRepository taskRepository = new TaskRepository();
     taskRepository.createTask(request);
+
+//    UpdateTaskRequest updateRequest= new UpdateTaskRequest();
+//    updateRequest.setDone(true);
+//
+//    taskRepository.updateTask(1,updateRequest);
+
+//    taskRepository.deleteTask(1);
+
+    List<Task> tasks = taskRepository.getTasks();
+    System.out.println(tasks);
   }
 }
--- src/main/java/org/example/persistance/TaskRepository.java
+++ src/main/java/org/example/persistance/TaskRepository.java
@@ -4,8 +4,14 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.example.domain.Task;
 import org.example.transfer.CreateTaskRequest;
+import org.example.transfer.UpdateTaskRequest;
 
 public class TaskRepository {
 
@@ -14,7 +20,7 @@ public class TaskRepository {
     String sql = "INSERT INTO task (description, deadline) VALUES (?, ?)";
 
     try (Connection connection = DatabaseConfiguration.getConnection();
-    PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
       preparedStatement.setString(1, request.getDescription());
       preparedStatement.setDate(2, Date.valueOf(request.getDeadline()));
@@ -23,4 +29,55 @@ public class TaskRepository {
     }
   }
 
+  public void updateTask(long id, UpdateTaskRequest request)
+      throws SQLException, IOException, ClassNotFoundException {
+    String sql = "UPDATE task SET done=? WHERE id=?";
+
+    try (Connection connection = DatabaseConfiguration.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+      preparedStatement.setBoolean(1, request.isDone());
+      preparedStatement.setLong(2, id);
+
+      preparedStatement.executeUpdate();
+    }
+  }
+
+  public void deleteTask(long id) throws SQLException, IOException, ClassNotFoundException {
+    String sql = "DELETE FROM task WHERE id = ?";
+    try (Connection connection = DatabaseConfiguration.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+      preparedStatement.setLong(1, id);
+
+      preparedStatement.executeUpdate();
+    }
+  }
+
+  public List<Task> getTasks() throws SQLException, IOException, ClassNotFoundException {
+    String sql = "SELECT id, description, deadline, done FROM task";
+
+    List<Task> tasks = new ArrayList<>();
+
+    try (Connection connection = DatabaseConfiguration.getConnection();
+        Statement statement = connection.createStatement()) {
+
+      ResultSet resultSet = statement.executeQuery(sql);
+
+      while (resultSet.next()) {
+        Task task = new Task();
+        task.setId(resultSet.getLong("id"));
+        task.setDescription(resultSet.getString("description"));
+
+        Date deadlineAsSqlDate = resultSet.getDate("deadline");
+        task.setDeadline(deadlineAsSqlDate.toLocalDate());
+
+        task.setDone(resultSet.getBoolean("done"));
+
+        tasks.add(task);
+      }
+    }
+
+    return tasks;
+  }
 }
